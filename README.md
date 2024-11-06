841f5218cf3768485a52e1282cc8b1335d366fe8
Tomás Antunes <tmsntns@gmail.com>
2024-11-06 01:02:41 +0000

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+Minimal Reproducible Repo for question https://stackoverflow.com/questions/79161011/why-is-nodehttp-behaving-unexpectedly-in-deno
+
+# Run
+
+- Run test-deno with deno using `deno -A main.ts`
+- Navigate to localhost
+- Check log
+- Run test-node with node using `node index.js`
+- Navigate to localhost
+- Check log
---
+++ test-node/index.js
@@ -0,0 +1,12 @@
+require("http").createServer((req, res) => {
+	console.log(req.rawHeaders)
+	console.log(req.rawHeaders.toString())
+	console.log(JSON.stringify(req.rawHeaders))
+	console.log(typeof req.rawHeaders)
+	console.log(Object.keys(req.rawHeaders))
+	console.log(Object.getOwnPropertyNames(req.rawHeaders))
+	console.log(Array.isArray(req.rawHeaders))
+	console.log(Object.getPrototypeOf(req.rawHeaders))
+	console.log("-------------")
+	res.end()
+}).listen(80)
